67f2b510f6f19f33e24ebc9d6dc0ee00e639c663
Om Bhor <ombhor617@gmail.com>
2025-03-15 16:36:13 +0530

    Frontend design

--- frontend/src/landing_page/pricing/PricingPage.jsx
+++ frontend/src/landing_page/pricing/PricingPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Hero from './Hero'
+import Brokerage from './Brokerage'
+import OpenAccount from '../OpenAccount'
 
 const PricingPage = () => {
   return (
-    <div>PricingPage</div>
+    <>
+      <Hero />
+      <OpenAccount />
+      <Brokerage />
+    </>
   )
 }
 
--- frontend/src/landing_page/support/SupportPage.jsx
+++ frontend/src/landing_page/support/SupportPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from './Hero'
+import CreateTicket from './CreateTicket'
 
 const SupportPage = () => {
   return (
-    <div>SupportPage</div>
+    <>
+      <Hero />
+      <CreateTicket />
+    </>
   )
 }
 
